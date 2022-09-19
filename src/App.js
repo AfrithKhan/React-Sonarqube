3dc0b12b7c7bd25a0343843fdd17f37b50a37a92
manoj_softsuave <manoj.rajendran@softsuave.com>
2022-09-19 23:20:14 +0530

    buildspec

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Changed
+          Edit <code>src/App.js</code> Again-Iam-Changed
         </p>
         <a
           className="App-link"
